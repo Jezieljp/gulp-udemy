b0455bb20736e4f07f9810b87a90717a8698e9ec
jezieljpl <cccccc157@hotmail.com>
2018-04-25 18:10:27 -0300

    configurando o express

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 var customizeBootstrap = require('gulp-customize-bootstrap');
-var gulpLess = require('gulp-less');
+var less = require('gulp-less');
 
 
 // compilando o bootstrap
 gulp.task('compileBootstrap', function() {
-    return gulp.src('node_modules/bootstrap/less/bootstrap.less')
-      .pipe(customizeBootstrap('/dev/style/less/*.less'))
+    return gulp.src('./node_modules/bootstrap/less/bootstrap.less')
+      .pipe(customizeBootstrap('./dev/styles/less/*.less'))
       .pipe(less())
-      .pipe(gulp.dest('dist/css'));
+      .pipe(gulp.dest('./dist/css/'));
   });
 
-gulp.task('default', ['compileBootstrap',''])
+gulp.task('default', ['compileBootstrap'])
